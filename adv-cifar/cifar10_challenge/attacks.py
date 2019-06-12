bdaa8d0925352f717d224206714247e17521e9dc
tianweiy <yintianwei@utexas.edu>
2019-06-12 16:01:58 -0500

    updates

--- adv-cifar/cifar10_challenge/attacks.py
+++ adv-cifar/cifar10_challenge/attacks.py
@@ -10,50 +10,28 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 import torch as ch
-
+import tensorflow_hub as hub
 
 def ce(y_, y):
     return tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
 
 class L2OPAttack:
-    def __init__(self, generator, model, epsilon, num_steps, step_size, random_start, loss_func):
+    def __init__(self):
         """overpowered attack using max-min optimization"""
-        self.model = model
-        self.epsilon = epsilon
-        self.num_steps = num_steps
-        self.step_size = step_size
-        self.rand = random_start
-        self.gen = generator
 
-        if loss_func == 'xent':
-            loss = model.xent
-        elif loss_func == 'cw':
-            label_mask = tf.one_hot(model.y_input,
-                                    10,
-                                    on_value=1.0,
-                                    off_value=0.0,
-                                    dtype=tf.float32)
-            correct_logit = tf.reduce_sum(label_mask * model.pre_softmax, axis=1)
-            wrong_logit = tf.reduce_max((1 - label_mask) * model.pre_softmax - 1e4 * label_mask, axis=1)
-            loss = -tf.nn.relu(correct_logit - wrong_logit + 50)
-        else:
-            print('Unknown loss function. Defaulting to cross-entropy')
-            loss = model.xent
-
-        self.grad = tf.gradients(loss, model.x_input)[0]
-
-    def perturb(self, sess, net, gan, eps, im_size=784, embed_feats=256, num_images=50, z_lr=5e-3, lambda_lr=1e-4,
-             num_steps=1000,
-             batch_num=None, ind=None):
+    def perturb(self, sess, net, gan, eps, embed_feats=256, num_images=50, z_lr=5e-3, lambda_lr=1e-4,
+             num_steps=1000):
 
         BATCH_SIZE = 1
 
-        batch1 = tf.zeros((num_images, 1, 28, 28))
-        batch2 = tf.zeros((num_images, 1, 28, 28))
+        batch1 = tf.zeros((num_images, 3, 28, 28))
+        batch2 = tf.zeros((num_images, 3, 28, 28))
         is_valid = tf.zeros(num_images)
         EPS = eps
+
         for i in range(num_images // BATCH_SIZE):
+            # sample two latent code
             z1 = tf.Variable(tf.random.uniform([BATCH_SIZE, embed_feats]))
             z2 = tf.Variable(tf.random.uniform([BATCH_SIZE, embed_feats]))
 
@@ -65,12 +43,19 @@ class L2OPAttack:
             opt2 = tf.train.GradientDescentOptimizer(lambda_lr)
 
             for j in range(num_steps):
-                x1 = gan(z1)
-                x2 = gan(z2)
+                # generate images
+                x1 = gan(z1, signature="generator")
+                x2 = gan(z2, signature="generator")
+
                 distance_mat = tf.norm(tf.reshape(x1 - x2, (x1.shape[0], -1)), axis=-1, keep_dims=False) - EPS * ones
 
-                net_res1 = sess.run(net.predictions, feed_dict={self.model.x_input: x1})
-                net_res2 = sess.run(net.predictions, feed_dict={self.model.x_input: x2})
+                # pre-softmax logits
+                net_pre1 = net.forward(x1)
+                net_pre2 = net.forward(x2)
+
+                # predictecd labels
+                net_res1 = tf.argmax(net_pre1, axis=-1)
+                net_res2 = tf.argmax(net_pre2, axis=-1)
 
                 is_adv = tf.cast(1 - (net_res1 == net_res2), tf.float32)
                 is_feasible = tf.cast((distance_mat <= 0), tf.float32)
@@ -81,10 +66,6 @@ class L2OPAttack:
                     is_valid[i * BATCH_SIZE:(i + 1) * BATCH_SIZE] = 1.
                     break
 
-                # pre-softmax logits
-                net_pre1 = sess.run(net.pre_softmax, feed_dict={self.model.x_input: x1})
-                net_pre2 = sess.run(net.pre_softmax, feed_dict={self.model.x_input: x2})
-
                 # calculate loss1, update opt1
                 loss1 = (-1.* tf.reduce_sum(ce(net_pre1, net_pre2), reduction_indices=None)*not_valid) +\
                         tf.reduce_sum(lambda_ * distance_mat*not_valid) + 1e-4*tf.reduce_sum(tf.norm(z1, axis=-1)*not_valid) +\
@@ -94,6 +75,9 @@ class L2OPAttack:
 
                 loss2 = -1. * tf.reduce_mean(lambda_ * distance_mat * not_valid)
                 opt2.minimize(loss2, var_list=[lambda_])
+
+                # update latent code
+                sess.run([opt1, opt2])
 
             batch1[i * BATCH_SIZE:(i + 1) * BATCH_SIZE, ...] = x1
             batch2[i * BATCH_SIZE:(i + 1) * BATCH_SIZE, ...] = x2
@@ -121,44 +105,34 @@ if __name__ == '__main__':
         sys.exit()
 
     model = Model(mode='eval')
-    attack = LinfPGDAttack(model,
-                           config['epsilon'],
-                           config['num_steps'],
-                           config['step_size'],
-                           config['random_start'],
-                           config['loss_func'])
+    attack = L2OPAttack()
     saver = tf.train.Saver()
-
-    data_path = config['data_path']
-    cifar = cifar10_input.CIFAR10Data(data_path)
 
     with tf.Session() as sess:
         # Restore the checkpoint
         saver.restore(sess, model_file)
 
         # Iterate over the samples batch-by-batch
-        num_eval_examples = config['num_eval_examples']
-        eval_batch_size = config['eval_batch_size']
-        num_batches = int(math.ceil(num_eval_examples / eval_batch_size))
+        num_train_examples = config['num_eval_examples']
+        train_batch_size = config['eval_batch_size']
+        num_batches = int(math.ceil(num_train_examples / train_batch_size))
 
         x_adv = []  # adv accumulator
 
         print('Iterating over {} batches'.format(num_batches))
 
-        for ibatch in range(num_batches):
-            bstart = ibatch * eval_batch_size
-            bend = min(bstart + eval_batch_size, num_eval_examples)
-            print('batch size: {}'.format(bend - bstart))
+        batch_size = 50
+        z_dim = 128
 
-            x_batch = cifar.eval_data.xs[bstart:bend, :]
-            y_batch = cifar.eval_data.ys[bstart:bend]
+        gan = hub.Module("https://tfhub.dev/google/compare_gan/model_13_cifar10_resnet_cifar/1")
 
-            x_batch_adv = attack.perturb(x_batch, y_batch, sess)
-
-            x_adv.append(x_batch_adv)
+        batch1, batch2, is_valid = attack.perturb(sess, model, gan, config['epsilon'])
 
         print('Storing examples')
         path = config['store_adv_path']
-        x_adv = np.concatenate(x_adv, axis=0)
+        batch1 = np.concatenate(batch1, axis=0)
         np.save(path, x_adv)
+
+        batch2 = np.concatenate(batch2, axis=0)
+        np.save(path, batch2)
         print('Examples stored in {}'.format(path))
